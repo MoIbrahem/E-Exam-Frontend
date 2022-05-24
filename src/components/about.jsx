b036c410d1cabb1b77f4e08507f67d3eba419d5d
hassan-prog <alielsayed1112@gmail.com>
2022-05-24 02:49:31 +0200

    added about component to navbar

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import { divide } from "lodash";
+import React, { Component } from "react";
+
+class About extends Component {
+  state = {};
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default About;
